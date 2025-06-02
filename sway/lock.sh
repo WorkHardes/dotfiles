8f302fd4bfe07120a1e076750c3fc6182542ee6a
WorkHardes <arckadyor34@gmail.com>
2025-06-02 09:46:00 +0300

    refactor: update sway monitor config

--- sway/lock.sh
+++ sway/lock.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
-swaylock \
-		--screenshots \
-		--clock \
-		--timestr "%H:%M:%S" \
-		--datestr "%A, %d-%m-%Y" \
-		--indicator-idle-visible \
-		--indicator-radius 110 \
-		--indicator-thickness 8 \
-		--fade-in 0.5 \
-		--ring-color 455a64 \
-		--key-hl-color be5046 \
-		--text-color ffc107 \
-		--line-color 00000000 \
-		--inside-color 00000088 \
-		--separator-color 00000000 \
-		--effect-blur 8x4 \
-		--effect-vignette 0.5:0.5
+# swaylock \
+# 		--screenshots \
+# 		--clock \
+# 		--timestr "%H:%M:%S" \
+# 		--datestr "%A, %d-%m-%Y" \
+# 		--indicator-idle-visible \
+# 		--indicator-radius 110 \
+# 		--indicator-thickness 8 \
+# 		--fade-in 0.5 \
+# 		--ring-color 455a64 \
+# 		--key-hl-color be5046 \
+# 		--text-color ffc107 \
+# 		--line-color 00000000 \
+# 		--inside-color 00000088 \
+# 		--separator-color 00000000 \
+# 		--effect-blur 8x4 \
+# 		--effect-vignette 0.5:0.5
+
+hyprlock -c $HOME/.config/hypr/hyprlock/hyprlock.conf
